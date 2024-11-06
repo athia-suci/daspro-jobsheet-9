2a87553b7ee9f5e8dae3b53533169ca4e3602cdc
NaufalAlifAdjani <naufalalifadjani12@gmail.com>
2024-11-06 19:15:16 +0700

    update menerima input berupa banyaknya elemen array, isi array, dan key yang ingin dicari serta output jika key tidak ada

--- LinearSearch16.java
+++ LinearSearch16.java
@@ -1,17 +1,33 @@
+import java.util.Scanner;
+
 public class LinearSearch16 {
     public static void main(String[] args) {
-        int arrayInt[] ={34, 18, 26, 48, 72, 20, 56,63};
-        int key = 20;
+        // int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
+        // int key = 90;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan jumlah elemen array : ");
+        int banyakNilai = sc.nextInt();
+
+        int[] arrNilai = new int[banyakNilai];
+        int key;
         int hasil = 0;
-        
-        for (int i = 0; i < arrayInt.length; i++){
-            if (arrayInt[i] == key) {
+
+        for (int i = 0; i < arrNilai.length; i++) {
+            System.out.print("Masukkan elemen array ke-" +i+ " : ");
+            arrNilai[i] = sc.nextInt();
+        }
+        System.out.print("Masukkan key yang ingin dicari : ");
+        key = sc.nextInt();
+        for (int i = 0; i < arrNilai.length; i++) {
+            if (key == arrNilai[i]) {
                 hasil = i;
+                System.out.println("Nilai " + key + " ada di posisi indeks ke-" + hasil);
+                break;
+            }else{
+                System.out.println("Key tidak ditemukan");
                 break;
             }
         }
-
-        System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
     }
 }
 
